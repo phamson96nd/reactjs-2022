74f84ab2da9ccbe3c3c08aad5c3a61ab046f89a7
son.pham <son.pham@kiaisoft.com>
2022-06-19 22:58:33 +0700

    practice useState in react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+
+const gifts = [
+  'CPU i9',
+  'Ram 32GB',
+  'RGB keyboard'
+];
+
+const courses = [
+  {id: 1, name: 'PHP'},
+  {id: 2, name: 'JS'},
+  {id: 3, name: 'Python'},
+]
 
 function App() {
+  const [gift, setGift] = useState()
+  const [name, setName] = useState('')
+  const [checked, setChecked] = useState([])
+
+  const randomGift = () => {
+    const index = Math.floor(Math.random() * gifts.length)
+
+    setGift(gifts[index])
+  }
+
+  const handleCheck = (id) => {
+    setChecked(prev => {
+      const isChecked = checked.includes(id)
+      if (isChecked) {
+        return checked.filter(item => item !== id)
+      } else {
+        return [...prev, id]
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper" style={{padding: 32}}>
+      <h1>{gift || 'Chưa có phần thưởng'}</h1>
+      <button onClick={randomGift}>Lấy thưởng</button> <br/>
+
+      <input type="text"
+        onChange={e => setName(e.target.value)}
+        value={name}
+      />
+
+      {courses.map((course) => (
+        <div key={course.id}>
+          <input
+            checked={checked.includes(course.id)}
+            onChange={() => handleCheck(course.id)}
+            type='checkbox'
+          />{course.name}
+        </div>
+      ))}
+
     </div>
   );
 }
